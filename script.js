40abad2de049f62e5957b2a9cdfc45a301847d9a
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-25 01:46:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+// Get references to the elements by their IDs
+const statusParagraph = document.getElementById("status");
+const enterButton = document.getElementById("enterBtn");
+
+// Add a click event listener to the button
+enterButton.addEventListener("click", function() {
+    // Create a new h1 element
+    const newHeading = document.createElement("h1");
+    
+    // Copy the text and attributes from the p tag to the new h1 tag
+    newHeading.textContent = statusParagraph.textContent;
+    
+    // Replace the p tag with the new h1 tag
+    statusParagraph.parentNode.replaceChild(newHeading, statusParagraph);
+});
